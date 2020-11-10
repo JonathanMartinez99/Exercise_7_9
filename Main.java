c67b24a9ef7f871ec729b9f2a33c7cb445e39b9b
JonathanMartinez99 <73108525+JonathanMartinez99@users.noreply.github.com>
2020-11-10 13:48:31 +0100

    Update Main.java
    I've added some comments

--- Main.java
+++ Main.java
@@ -13,7 +13,7 @@ public class Main {
         System.out.println("Insert a mark: ");
         endCheck = scan.nextLine();
 
-        while(!endCheck.equals("end"))
+        while(!endCheck.equals("end")) // We keep asking notes, as far as endCheck is different from "end".
         {
             userNote = Double.parseDouble(endCheck);
             numberCounter++;
@@ -22,7 +22,7 @@ public class Main {
             endCheck = scan.nextLine();
         }
 
-        if (numberCounter != 0)
+        if (numberCounter != 0) // We calculate and show the notes average.
         {
             notesAverage = notesSummatory / numberCounter;
             System.out.println("The average is : " + notesAverage);
